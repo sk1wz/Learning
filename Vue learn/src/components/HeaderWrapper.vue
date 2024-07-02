849e059e725e1638db17313d472450a77a09853e
sk1wz <78929376+sk1wz@users.noreply.github.com>
2024-07-02 23:36:49 +0300

    Vue first project

---
+++ Vue learn/src/components/HeaderWrapper.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  sortBy: String,
+  search: String,
+  onChangeSelect: Function,
+  onChangeInput: Function
+})
+
+</script>
+
+<template>
+    <div class="headerWrapper flex justify-between items-center">
+      <h2 class="text-3xl font-bold m-4">Все кроссовки</h2>
+
+      <div class="searching flex justify-center items-center relative">
+        <img src="../../public/search.svg" class="absolute left-6 top-6" alt="">
+        <input type="text" @input="onChangeInput" placeholder="Поиск..." class="pl-10 border border-slate-200 p-1 rounded outline-none m-4 cursor-pointe focus:border-gray-400" />
+        <select @change="onChangeSelect" class="m-4 border border-slate-200 p-1 rounded outline-none cursor-pointer">
+          <option value="name">По названию</option>
+          <option value="price">По цене (Дешевые)</option>
+          <option value="-price">По цене (Дорогие)</option>
+        </select>
+      </div>
+
+    </div>
+</template>
+
+<style scoped>
+@media(width < 900px){
+  .headerWrapper{
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+  }
+  
+}
+@media(width < 600px){
+  .searching{
+    flex-direction: column;
+    align-items: flex-start;
+  }
+}
+</style>
---
+++ Vue learn/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import App from './App.vue'
+const app = createApp(App)
+app.use(autoAnimatePlugin)
+app.mount('#app')
